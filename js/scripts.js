b962f0b16e052bf25a17034808907914aedb7bf8
Guest User <Guest@Epicodus-5A.local>
2017-06-21 19:32:18 -0700

    add images to make site more appealing

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,32 @@
+
+// user interface logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").text(result);
-    
+
+    $(".year").text(year);
+
+       if (!result) {          
+         $(".not").text("not");
+       } else {
+         $(".not").text("");
+       }
+
+       $("#result").show();
   });
 });
 
+//business logic
 var leapYear = function(year) {
-  return false;
-};
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};;
+
 
 // jQuery user interface logic here.
